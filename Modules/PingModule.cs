e5b306c1c5acb0d38c9a943bea8922600f4f7681
drobbins329 <drobbins329@gmail.com>
2022-02-03 19:16:55 -0500

    Basic ping slash command implemented

---
+++ Modules/PingModule.cs
@@ -0,0 +1,26 @@
+﻿using Discord.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNet_V3_Tutorial
+{
+    public class PingModule : InteractionModuleBase<SocketInteractionContext>
+    {
+        public InteractionService Commands { get; set; }
+
+        private CommandHandler _handler;
+        public PingModule(CommandHandler handler)
+        {
+            _handler = handler;
+        }
+
+        [SlashCommand("ping", "Receive a pong!")]
+        public async Task Ping()
+        {
+            await RespondAsync("pong");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,30 +1,43 @@
 ﻿using Discord.Interactions;
 using Discord.WebSocket;
 using DNet_V3_Bot.Logger;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration.Yaml;
+using YamlDotNet;
 using System;
+using System.Collections;
+using DNet_V3_Bot;
 
 namespace DNet_V3_Tutorial
 {
     public class program
     {
-        private DiscordSocketClient _client;
+        private DiscordSocketClient _client;        
         
         public static Task Main(string[] args) => new program().MainAsync();
 
         public async Task MainAsync()
         {
+            var config = new ConfigurationBuilder()
+            .AddEnvironmentVariables(prefix: "&")
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddYamlFile("config.yml")
+            .Build();
+            
             using IHost host = Host.CreateDefaultBuilder()
                 .ConfigureServices((_, services) =>
             services
             //.AddTransient<IConsoleLogger, Logger>()
+            .AddSingleton(config)
             .AddSingleton(x => new DiscordSocketClient(new DiscordSocketConfig
             {
                 GatewayIntents = Discord.GatewayIntents.AllUnprivileged,
                 AlwaysDownloadUsers = true,
             }))
             .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
+            .AddSingleton<CommandHandler>()
             .AddTransient<ConsoleLogger>())
             .Build();
 
@@ -44,17 +57,36 @@ namespace DNet_V3_Tutorial
 
             var commands = provider.GetRequiredService<InteractionService>();
             _client = provider.GetRequiredService<DiscordSocketClient>();
-            
+            var config = provider.GetRequiredService<IConfigurationRoot>();
+
+            await provider.GetRequiredService<CommandHandler>().InitializeAsync();
+
             _client.Log += _ => provider.GetRequiredService<ConsoleLogger>().Log(_);
             commands.Log += _ => provider.GetRequiredService<ConsoleLogger>().Log(_);
 
+            _client.Ready += async () =>
+            {
+                if (IsDebug())
+                    // Id of the test guild can be provided from the Configuration object
+                    await commands.RegisterCommandsToGuildAsync(UInt64.Parse(config["testGuild"]), true);
+                else
+                    await commands.RegisterCommandsGloballyAsync(true);
+            };
 
-            var token = "NzI2MjQxODgwMTY5MjUwOTE5.XvabdQ.GOUUFV6cO6R6RPGNLKbcMs_zJxw";
-            await _client.LoginAsync(Discord.TokenType.Bot, token);
+
+            await _client.LoginAsync(Discord.TokenType.Bot, config["tokens:discord"]);
             await _client.StartAsync();
 
             await Task.Delay(-1);
         }
 
+        static bool IsDebug()
+        {
+#if DEBUG
+            return true;
+#else
+            return false;
+#endif
+        }
     }
 }
