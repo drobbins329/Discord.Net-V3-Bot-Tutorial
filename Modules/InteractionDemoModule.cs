672a289193366c654275be24cfef242678558df3
KeylAmi <drobbins329@gmail.com>
2022-02-13 10:05:45 -0500

    Create InteractionDemoModule.cs

---
+++ Modules/InteractionDemoModule.cs
@@ -0,0 +1,76 @@
+﻿using Discord;
+using Discord.Interactions;
+using DNet_V3_Tutorial.Log;
+
+namespace DNet_V3_Tutorial
+{
+    // Must use InteractionModuleBase<SocketInteractionContext> for the InteractionService to auto-register the commands
+    public class InteractionDemoModule : InteractionModuleBase<SocketInteractionContext>
+    {
+        public InteractionService Commands { get; set; }
+        private static Logger _logger;
+
+        public InteractionDemoModule(ConsoleLogger logger)
+        {
+            _logger = logger;
+        }
+
+        // Simple slash command to bring up a message with a button to press
+        [SlashCommand("button", "Button demo command")]
+        public async Task ButtonInput()
+        {
+            var components = new ComponentBuilder();
+            var button = new ButtonBuilder()
+            {
+                Label = "Button",
+                CustomId = "button1",
+                Style = ButtonStyle.Primary
+            };
+
+            // Messages take component lists. Either buttons or select menus. The button can not be directly added to the message. It must be added to the ComponentBuilder.
+            // The ComponentBuilder is then given to the message components property.
+            components.WithButton(button);
+
+            await RespondAsync("This message has a button!", components: components.Build());
+        }
+
+        // This is the handler for the button created above. It is triggered by nmatching the customID of the button.
+        [ComponentInteraction("button1")]
+        public async Task ButtonHandler()
+        {
+            // try setting a breakpoint here to see what kind of data is supplied in a ComponentInteraction.
+            var c = Context;
+            await RespondAsync($"You pressed a button!");
+        }
+
+        // Simple slash command to bring up a message with a select menu
+        [SlashCommand("menu", "Select Menu demo command")]
+        public async Task MenuInput()
+        {
+            var components = new ComponentBuilder();
+            // A SelectMenuBuilder is created
+            var select = new SelectMenuBuilder()
+            {
+                CustomId = "menu1",
+                Placeholder = "Select something"
+            };
+            // Options are added to the select menu. The option values can be generated on execution of the command. You can then use the value in the Handler for the select menu
+            // to determine what to do next. An example would be including the ID of the user who made the selection in the value.
+            select.AddOption("abc", "abc_value");
+            select.AddOption("def", "def_value");
+            select.AddOption("ghi", "ghi_value");
+
+            components.WithSelectMenu(select);
+
+            await RespondAsync("This message has a menu!", components: components.Build());
+        }
+
+        // SelectMenu interaction handler. This receives an array of the selections made.
+        [ComponentInteraction("menu1")]
+        public async Task MenuHandler(string[] selections)
+        {
+            // For the sake of demonstration, we only want the first value selected.
+            await RespondAsync($"You selected {selections.First()}");
+        }
+    }
+}
