6b52f33562e2f33ab7ea138028312d3dbd18c23e
KeylAmi <drobbins329@gmail.com>
2022-02-27 14:28:30 -0500

    stripped all non-necessary code for basic bot

--- Program.cs
+++ Program.cs
@@ -1,11 +1,6 @@
-﻿using Discord.Interactions;
-using Discord.WebSocket;
-using DNet_V3_Tutorial.Log;
-using Microsoft.Extensions.Configuration;
+﻿using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Configuration.Yaml;
-using Discord.Commands;
 using Discord;
 
 namespace DNet_V3_Tutorial
@@ -18,39 +13,16 @@ namespace DNet_V3_Tutorial
         public static Task Main(string[] args) => new program().MainAsync();
 
         public async Task MainAsync()
-        {
-            var config = new ConfigurationBuilder()
-            // this will be used more later on
-            .SetBasePath(AppContext.BaseDirectory)
-            // I chose using YML files for my config data as I am familiar with them
-            .AddYamlFile("config.yml")
-            .Build();
-            
+        {            
             using IHost host = Host.CreateDefaultBuilder()
                 .ConfigureServices((_, services) =>
             services
-            // Add the configuration to the registered services
-            .AddSingleton(config)
             // Add the DiscordSocketClient, along with specifying the GatewayIntents and user caching
             .AddSingleton(x => new DiscordSocketClient(new DiscordSocketConfig
             {
                 GatewayIntents = Discord.GatewayIntents.AllUnprivileged,
                 AlwaysDownloadUsers = true,
-            }))
-			// Adding console logging
-            .AddTransient<ConsoleLogger>()
-            // Used for slash commands and their registration with Discord
-            .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
-            // Required to subscribe to the various client events used in conjunction with Interactions
-            .AddSingleton<InteractionHandler>()
-            // Adding the prefix Command Service
-            .AddSingleton(x => new CommandService(new CommandServiceConfig
-            {
-                LogLevel = LogSeverity.Debug,
-                DefaultRunMode = Discord.Commands.RunMode.Async
-            }))
-            // Adding the prefix command handler
-            .AddSingleton<PrefixHandler>())
+            })))
             .Build();
 
             await RunAsync(host);
@@ -61,47 +33,21 @@ namespace DNet_V3_Tutorial
             using IServiceScope serviceScope = host.Services.CreateScope();
             IServiceProvider provider = serviceScope.ServiceProvider;
 
-            var commands = provider.GetRequiredService<InteractionService>();
             _client = provider.GetRequiredService<DiscordSocketClient>();
-            var config = provider.GetRequiredService<IConfigurationRoot>();
-
-            await provider.GetRequiredService<InteractionHandler>().InitializeAsync();
-
-            var prefixCommands = provider.GetRequiredService<PrefixHandler>();
-            prefixCommands.AddModule<DNet_V3_Bot.Modules.PrefixModule>();
-            await prefixCommands.InitializeAsync();
-
 
             // Subscribe to client log events
-            _client.Log += _ => provider.GetRequiredService<ConsoleLogger>().Log(_);
-            // Subscribe to slash command log events
-            commands.Log += _ => provider.GetRequiredService<ConsoleLogger>().Log(_);
+            _client.Log += async (LogMessage msg) => { Console.WriteLine(msg.Message); };
 
             _client.Ready += async () =>
             {
-                // If running the bot with DEBUG flag, register all commands to guild specified in config
-                if (IsDebug())
-                    // Id of the test guild can be provided from the Configuration object
-                    await commands.RegisterCommandsToGuildAsync(UInt64.Parse(config["testGuild"]), true);
-                else
-                    // If not debug, register commands globally
-                    await commands.RegisterCommandsGloballyAsync(true);
+                Console.WriteLine("Bot ready!");
             };
 
 
-            await _client.LoginAsync(Discord.TokenType.Bot, config["tokens:discord"]);
+            await _client.LoginAsync(Discord.TokenType.Bot, "NzI2MjQxODgwMTY5MjUwOTE5.XvabdQ.2M9oa5Zj3RugZi2U1F3nMZgm8nA");
             await _client.StartAsync();
 
             await Task.Delay(-1);
-        }
-
-        static bool IsDebug()
-        {
-#if DEBUG
-            return true;
-#else
-            return false;
-#endif
         }
     }
 }
